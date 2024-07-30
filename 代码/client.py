934e98712cc31e71059026da5d1ea332e61defa6
ycy20090311 <12263881+ycy20090311@user.noreply.gitee.com>
2024-07-30 05:50:10 +0000

    update 代码/client.py.
    Signed-off-by: ycy20090311 <12263881+ycy20090311@user.noreply.gitee.com>

--- 代码/client.py
+++ 代码/client.py
@@ -4,10 +4,15 @@ from the_uuids import UUID as UUIDs
 
 uuids = UUIDs()
 
+
 def match_service_uuid(device: BLEDevice, adv: AdvertisementData):
     if uuids.service_uuid.lower() in adv.service_uuids:
         return True
     return False
+
+
+def handle_notification(sender, data):
+    print(f"Notification received: {data.decode('utf-8')}")
 
 
 async def main():
@@ -16,19 +21,13 @@ async def main():
 
     # 创建BleakClient客户端，连接后进行串口操作
     async with BleakClient(device) as client:
-
         print("Connected")
 
         service = client.services.get_service(uuids.service_uuid)
         # 接收蓝牙串口信息
         char = service.get_characteristic(uuids.characteristic_uuid)
+        await client.start_notify(char, handle_notification)
         await client.write_gatt_char(char, "吃了吗".encode("utf-8"))
 
-        # 新增notify功能 2024/07/30
-        await client.start_notify(char, handle_notification)
-
-# 接收蓝牙串口信息回调函数 2024/07/30
-def handle_notification(sender, data):
-    print(f"Notification received: {data.decode('utf-8')}")
 
 asyncio.run(main())
